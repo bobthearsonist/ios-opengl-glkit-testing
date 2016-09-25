17d8a5e9567710c1e47f7d4de8e2d0f3e2ec9f8e
Martin Perry <martinperry@gatech.edu>
2016-09-25 14:02:17 -0400

    New project for camera work

---
+++ CameraInContainer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraInContainer
+//
+//  Created by admin on 9/25/16.
+//  Copyright © 2016 me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
